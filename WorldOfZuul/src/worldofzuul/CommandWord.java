7ba44e771312442534b540a301ec56ef9afd2fad
Kasper <Kasperjalris@hotmail.com>
2019-10-30 10:42:01 +0100

    Spillet fungeere med minemale ændirnger

--- WorldOfZuul/src/worldofzuul/CommandWord.java
+++ WorldOfZuul/src/worldofzuul/CommandWord.java
@@ -3,8 +3,6 @@ package worldofzuul;
 public enum CommandWord
 {
 
-    GO, QUIT, HELP, UNKNOWN, PICKUP, TALK, A, B, C, D; 
-
     GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), PICKUP("pickup"), TALK("talk"), A("a"), B("b"), C("c"), D("d");
 
     
