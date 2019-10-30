f2dbb1cf698128727e7c64bbf51b95aa23cc7069
ipak95 <daniel.ipakchian@gmail.com>
2019-10-30 12:19:11 +0100

    player class

---
+++ WorldOfZuul/src/worldofzuul/Player.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author daniel
+ */
+public class Player {
+    
+    private ArrayList<Item> inventory = new ArrayList<Item>();
+}
+
