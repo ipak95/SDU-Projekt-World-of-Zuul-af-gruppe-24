6d7a4b09e6c2aa6d0d27b05ef93c2b5379123324
kechr19 <kechr19@student.sdu.dk>
2019-10-30 10:42:31 +0100

    Updating

--- WorldOfZuul/src/worldofzuul/CommandWord.java
+++ WorldOfZuul/src/worldofzuul/CommandWord.java
@@ -1,22 +1,18 @@
 package worldofzuul;
 
-public enum CommandWord
-{
+public enum CommandWord {
 
     GO, QUIT, HELP, UNKNOWN, PICKUP, TALK, A, B, C, D; 
 
     GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), PICKUP("pickup"), TALK("talk"), A("a"), B("b"), C("c"), D("d");
 
-    
     private String commandString;
     
-    CommandWord(String commandString)
-    {
+    CommandWord(String commandString) {
         this.commandString = commandString;
     }
-    
-    public String toString()
-    {
+
+    public String toString() {
         return commandString;
     }
 }
