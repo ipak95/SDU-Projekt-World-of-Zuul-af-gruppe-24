f6a6ac6e6c19f1662e97c93698d07ce3009582dd
Kechr <kechr19@student.sdu.dk>
2019-10-31 20:13:13 +0100

    Test

--- WorldOfZuul/src/worldofzuul/WorldOfZuul.java
+++ WorldOfZuul/src/worldofzuul/WorldOfZuul.java
@@ -4,7 +4,7 @@ public class WorldOfZuul {
 
     public static void main(String[] args) {
 
-        // Create Game instant and play the game
+        // Create Game instance and play the game
         Game game = new Game();
         game.play();
     }
