8d07c636078e1f407ab44ed5687c8789bc3974ca
Sebastian <sebab@123dk.dk>
2019-11-06 12:56:37 +0100

    Test
    GitKraken Test

--- WorldOfZuul/src/worldofzuul/WorldOfZuul.java
+++ WorldOfZuul/src/worldofzuul/WorldOfZuul.java
@@ -4,6 +4,7 @@ public class WorldOfZuul {
 
     public static void main(String[] args) {
 
+        // GitKraken test from Sebs computer
         // Create Game instance and play the game
         Game game = new Game();
         game.play();
