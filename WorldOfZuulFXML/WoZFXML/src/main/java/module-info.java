0941e9fd95ac6919d6c514a85298a3bf6093c2ad
kechr19 <kechr19@student.sdu.dk>
2019-11-26 11:02:31 +0100

    Panes virker

--- WorldOfZuulFXML/WoZFXML/src/main/java/module-info.java
+++ WorldOfZuulFXML/WoZFXML/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.wozfxml {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.wozfxml to javafx.fxml;
     exports com.mycompany.wozfxml;
