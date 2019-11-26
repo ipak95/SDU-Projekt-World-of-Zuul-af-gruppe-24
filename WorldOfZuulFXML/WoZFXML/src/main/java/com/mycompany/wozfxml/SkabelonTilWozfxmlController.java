5aa65890e2d89cc402c33a830d03427440aa6f45
kechr19 <kechr19@student.sdu.dk>
2019-11-26 11:03:08 +0100

    Panes virker

---
+++ WorldOfZuulFXML/WoZFXML/src/main/java/com/mycompany/wozfxml/SkabelonTilWozfxmlController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.wozfxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kenneth
+ */
+public class SkabelonTilWozfxmlController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
